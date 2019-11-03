ded882c63b989e12ccc48e9ee453e86e1b23b303
Hanna Lee <mani21na@gmail.com>
2019-11-03 22:10:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,20 +5,18 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
-      erb :super_hero
+    erb :team
   end
-
-
+  
   post '/teams' do
-    @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-    members = params[:team][:members]
-
-    members.each do |member_params|
-      Hero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
-    end
-      @super_heroes = SuperHero.all
-
-      erb :team
-    end
-end	
+    @team = Team.new(params[:team])
+    
+    params[:team][:members].each do |hero|
+      Hero.new(hero)
+    end 
+    
+    @hero = Hero.all 
+    
+    erb :super_hero
+  end
 end
