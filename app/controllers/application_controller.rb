e305aeee65aeb30fe8d0205cf3b0941b7e374aab
Hanna Lee <mani21na@gmail.com>
2019-11-03 21:45:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class App < Sinatra::Base
   post '/teams' do
     @team = Team.new(params[:team])
     
-    params[:team][:]
+    params[:team][:hero].each do |hero|
+      
+    end 
   end
 end
